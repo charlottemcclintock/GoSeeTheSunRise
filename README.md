001e898b93eb6977e3cc06c0846af08991e2b2be
Rohit Musti <rohit.musti.rm@gmail.com>
2021-11-01 17:34:48 -0400

    adding two factor auth, working version of ics

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 I wanted a way to have sunrise and sunsets on my calendar so I would go up to my roof and see the sun rise or set.
 
 data from [Sunrise Sunset API](https://sunrise-sunset.org/api)
+
+## ideas for mods/extentions:
+
+- add sun emoji to title
+- add fun sunny phrases to description
+- make downloadable/addable online?
--- cal.py
+++ cal.py
@@ -1,9 +1,40 @@
 
-from dotenv import load_dotenv
-import os
+from ics import Calendar, Event
+import pandas as pd
+from datetime import datetime
 
-load_dotenv('.env')
+# read in sunrise/sunset data
+sunnov = pd.read_csv('sun-patterns-nov1jun1.csv')
+sunjun = pd.read_csv('sun-patterns-jun2dec31.csv')
 
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
+# append together
+sun = sunnov.append(sunjun)
+
+# wide to long
+sun = pd.melt(sun, id_vars='date', value_vars=['sunrise', 'sunset'])
+
+# capitalize sunrise/sunset
+sun['variable'] = sun['variable'].str.title()
+
+# slice time string to get a pretty time for event titles
+sun['shorttime'] = sun['value'].str.slice(11,16)
+
+# reformat to 12 hour clock
+sun['shorttime'] = sun['shorttime'].map(lambda a: datetime.strptime(a, "%H:%M"))
+sun['shorttime'] = sun['shorttime'].map(lambda a: datetime.strftime(a, "%I:%M %p"))
+
+sun['title'] = sun['shorttime'] + " " + sun['variable']
+
+c = Calendar()
+
+for i, row in sun.head(5).iterrows():
+    e = Event()
+    e.name = row['title']
+    e.begin = row['value']
+    c.events.add(e)
+
+c.events
+
+with open('sun.ics', 'w') as my_file:
+    my_file.writelines(c)
 
