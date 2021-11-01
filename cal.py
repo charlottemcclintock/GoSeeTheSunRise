6c118b938b86ecc06e82c988e43a7eee125c6e2c
Rohit Musti <rohit.musti.rm@gmail.com>
2021-11-01 17:34:48 -0400

    adding two factor auth

--- cal.py
+++ cal.py
@@ -1,9 +1,16 @@
-
+from pyicloud import PyiCloudService
 from dotenv import load_dotenv
 import os
 
-load_dotenv('.env')
-
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
+if __name__=="__main__":
+  load_dotenv('.env')
+  
+  USERNAME = os.environ.get("USERNAME")
+  PASSWORD = os.environ.get("PASSWORD")
+  api = PyiCloudService('rohit.musti.rm@gmail.com', '9n3n7m%$mzbbf87PL#PVmdnk$I3^&wf')
+  if api.requires_2fa:
+        print("Two-factor authentication required.")
+        code = input("Enter the code you received of one of your approved devices: ")
+        result = api.validate_2fa_code(code)
+        print(f"Code validation result: {result}")
 
